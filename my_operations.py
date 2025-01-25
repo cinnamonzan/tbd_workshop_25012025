cab856b318c53706c4b08159a54e7fa44aac4a45
cinnamonzan <tanyalak_l@cmu.ac.th>
2025-01-25 14:12:39 +0700

    add

--- my_operations.py
+++ my_operations.py
@@ -1,9 +1,9 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
     # Edit code under this line
-    
+    res = num1 + num2
     print(num1, "+", num2, "is", res)
-    pass
+    
 
 
 # Member B: edit this function.
